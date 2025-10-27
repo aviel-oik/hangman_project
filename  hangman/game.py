ad8eed505be328f64e1e5949fb81177cba772df6
Aviel Oiknine <avieloikpro@gmail.com>
2025-10-27 09:48:20 +0200

    func : validate_guess

---  hangman/game.py
+++  hangman/game.py
@@ -4,3 +4,18 @@ def init_state(secret: str, max_tries: int) -> dict:
     display_list = ["_"] * len(secret)
     dic = {"secret" : secret, "display" : display_list, "guesses" : [], "wrong_guesses" : 0, "max_tries" : max_tries}
     return dic
+
+def validate_guess(ch: str, guessed: set[str]) -> tuple[bool, str]:
+    valid = True
+    if len(ch) != 1:
+        return False,"invalid input"
+    if ch in guessed:
+        return False,"already guessed,try again"
+    else:
+# guessed.add(ch)
+        return True,"check..."
+
+
+
+
+
